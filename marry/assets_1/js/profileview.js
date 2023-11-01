7a77bf9457f396f66cc23586c21005fe4cbc6bfa
ehas1990 <ehasahamed@gmail.com>
2023-11-01 17:38:45 +0530

    view profile completed

--- marry/assets_1/js/profileview.js
+++ marry/assets_1/js/profileview.js
@@ -18,3 +18,24 @@ function goBack() {
     document.getElementById('openButton').style.display = 'block';
     document.getElementById('displayText').style.display = 'none';
 }
+// Basic 
+function informationEditor() {
+    document.getElementById('openButton1').style.display = 'none';
+    document.getElementById('basiceditor').style.display = 'block';
+}
+
+function infosaveText() {
+    const textareaValue = document.getElementById('textarea_basicinfo').value;
+    document.getElementById('BasicInformation').textContent =  textareaValue;
+    document.getElementById('BasicInformation').style.display = 'block';
+
+    // Hide the textarea, Save, and Back buttons
+    document.getElementById('basiceditor').style.display = 'none';
+    document.getElementById('openButton1').style.display = 'block';
+}
+
+function infoBack() {
+    document.getElementById('basiceditor').style.display = 'none';
+    document.getElementById('openButton1').style.display = 'block';
+    document.getElementById('BasicInformation').style.display = 'none';
+}
